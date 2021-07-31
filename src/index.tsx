cc2ccd35a188d1d32fed404bd28b9545e95daf3a
Yuki Takagi <takagiy.4dev@gmail.com>
2021-07-31 22:33:36 +0900

    Add Academico.otf for the serif font in the scores

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./fonts/Bravura.otf";
+import "./fonts/AcademicoRegular.otf";
 import { Editor } from "./Editor";
 import reportWebVitals from "./reportWebVitals";
 
