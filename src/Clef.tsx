7bcb4c100b32f0052a0d6439002d8ddc8e839d0e
Yuki Takagi <takagiy.4dev@gmail.com>
2021-08-01 01:20:53 +0900

    Switch kind of the clefs with the property

--- src/Clef.tsx
+++ src/Clef.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import _ from "lodash";
 
 type Props = {
   kind: "g-clef" | "f-clef";
@@ -8,7 +7,7 @@ type Props = {
 export const Clef = (props: Props) => {
   return (
     <text x="20" y={props.kind === "g-clef" ? "75" : "50"}>
-      {props.kind === "g-clef" ? "&#xE050;" : "&#xE062;"}
+      {props.kind === "g-clef" ? "\u{E050}" : "\u{E062}"}
     </text>
   );
 };
