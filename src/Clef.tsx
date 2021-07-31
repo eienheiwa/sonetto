c3c9d7e5da537aa51d2fbcc38b035d03aa87442f
Yuki Takagi <takagiy.4dev@gmail.com>
2021-08-01 02:42:40 +0900

    Fix y coordinate of f-clef

--- src/Clef.tsx
+++ src/Clef.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 type Props = {
-  kind: "g-clef" | "f-clef";
+  kind: Kind;
 };
+
+export type Kind = "g-clef" | "f-clef";
 
 export const Clef = (props: Props) => {
   return (
-    <text x="20" y={props.kind === "g-clef" ? "75" : "50"}>
+    <text x="20" y={props.kind === "g-clef" ? "75" : "25"}>
       {props.kind === "g-clef" ? "\u{E050}" : "\u{E062}"}
     </text>
   );
