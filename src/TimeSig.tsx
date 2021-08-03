8a87b578f721e75634a3c8a0dd055f060b545c96
Yuki Takagi <takagiy.4dev@gmail.com>
2021-08-04 02:09:17 +0900

    code format

--- src/TimeSig.tsx
+++ src/TimeSig.tsx
@@ -6,17 +6,28 @@ type Props = {
   bottom: number;
 };
 
-const glyphs = ['\u{E080}','\u{E081}','\u{E082}','\u{E083}','\u{E084}','\u{E085}','\u{E086}','\u{E087}','\u{E088}','\u{E089}'];
+const glyphs = [
+  "\u{E080}",
+  "\u{E081}",
+  "\u{E082}",
+  "\u{E083}",
+  "\u{E084}",
+  "\u{E085}",
+  "\u{E086}",
+  "\u{E087}",
+  "\u{E088}",
+  "\u{E089}",
+];
 
 export const TimeSig = (props: Props) => {
   let x = props.x || 0;
   return (
     <>
       <text x={x} y="25" textAnchor="middle">
-          {glyphs[props.top]}
-          </text>
+        {glyphs[props.top]}
+      </text>
       <text x={x} y="75" textAnchor="middle">
-          {glyphs[props.bottom]}
+        {glyphs[props.bottom]}
       </text>
     </>
   );
